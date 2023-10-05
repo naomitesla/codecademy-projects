ddcb870a62c133408263d0c6472ba527e358182f
naomi tesla <naomitesla@protonmail.com>
2023-10-05 11:39:53 -0400

    added lil ascii frennsss

---
+++ Learn Go/ascii_art.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    f "fmt"
+)
+
+func main(){
+    f.Println("      `.-::::::-.`")
+    f.Println("  .:-::::::::::::::-:.")
+    f.Println("  `_:::    ::    :::_`")
+    f.Println("   .:( ^   :: ^   ):.")
+    f.Println("   `:::   (..)   :::.")
+    f.Println("   `:::::::UU:::::::`")
+    f.Println("   .::::::::::::::::.")
+    f.Println("   O::::::::::::::::O")
+    f.Println("   -::::::::::::::::-")
+    f.Println("   `::::::::::::::::`")
+    f.Println("    .::::::::::::::.")
+    f.Println("      oO:::::::Oo")
+
+    f.Println(`
+         __      _
+        o''}____//
+         ` + "`" + `_/      )
+         (_(_/-(_/
+    `)
+}
